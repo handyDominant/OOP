e530a8fecb92d08f50d74e2d85134bad88f00a6a
handyDominant <acsic666@mail.ru>
2023-10-23 19:57:17 +0300

    First added page

--- 15/app.js
+++ 15/app.js
@@ -23,7 +23,64 @@
 // ]
 
 
-
+const data = [
+    { "id": "javascript", "label": "JavaScript", "category": "programmingLanguages", "priority": 1 },
+    { "id": "typescript", "label": "TypeScript", "category": "programmingLanguages", "priority": 1 },
+    { "id": "sql", "label": "SQL", "category": "programmingLanguages", "priority": 2 },
+    { "id": "java", "label": "Java", "category": "programmingLanguages", "priority": 3 },
+    { "id": "go", "label": "GO", "category": "programmingLanguages", "priority": 3 }
+  ];
+  
+  class ServerDelete {
+    constructor() {
+      this.data = data;
+    }
+  
+    // Middleware для валидации JSON-запроса
+    middleware(request) {
+      if (!request.id) {
+        throw new Error('Invalid JSON request: "id" field is required.');
+      }
+    }
+  
+    // Контроллер для обработки JSON-запроса
+    controller(request) {
+      this.middleware(request); // Вызываем middleware для валидации
+      const idToDelete = request.id;
+      return this.service(idToDelete);
+    }
+  
+    // Сервис для выполнения операции удаления и проверки
+    service(idToDelete) {
+      const itemIndex = this.data.findIndex(item => item.id === idToDelete);
+      if (itemIndex === -1) {
+        throw new Error(`Item with ID "${idToDelete}" not found.`);
+      }
+  
+      // Выполняем удаление элемента из массива данных
+      const deletedItem = this.data.splice(itemIndex, 1)[0];
+      return deletedItem;
+    }
+  
+    // Репозиторий для симуляции БД
+    repository() {
+      // Уже реализовано выше, так как мы работаем с массивом данных.
+    }
+  }
+  
+  // Пример использования класса ServerDelete
+  const serverDelete = new ServerDelete();
+  const request = {
+    id: "javascript"
+  };
+  
+  try {
+    const deletedItem = serverDelete.controller(request);
+    console.log(`Item with ID "${deletedItem.id}" deleted successfully.`);
+    console.log('Updated data:', serverDelete.data);
+  } catch (error) {
+    console.error(error.message);
+  }
 
 
 
